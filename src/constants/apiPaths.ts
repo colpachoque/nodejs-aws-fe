0363cc35436044231a768a55b39d1298bd700a05
Olga Khamanina <smile-94@inbox.ru>
2020-11-15 15:32:45 +0300

    Fixed import

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://5yiud9skc2.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://kb3q4g2bxc.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://5yiud9skc2.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
