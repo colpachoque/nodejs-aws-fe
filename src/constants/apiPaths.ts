8bd06a7f1dff0f7a44cf00fbe5d58a4391c4d29a
Herman_Bilous <Herman_Bilous@epam.com>
2020-11-29 23:00:38 +0200

    Add cart api integration

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import2: 'https://kb3q4g2bxc.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://m2aacpogv2.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://5yiud9skc2.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://5yiud9skc2.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://colpachoque-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
